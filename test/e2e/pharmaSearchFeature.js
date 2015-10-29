f4e218f3368a30a593a1b113f8dd988ed8519672
Toby Clarke <teeohbee@gmail.com>
2015-10-29 13:56:38 +0000

    Add test for displaying results list

--- test/e2e/pharmaSearchFeature.js
+++ test/e2e/pharmaSearchFeature.js
@@ -1,7 +1,15 @@
 describe('Pharma Search', function() {
+  var resultslist = element(by.css('.resultslist'));
+
  it('has a title', function() {
    browser.get('http://localhost:8080');
 
    expect(browser.getTitle()).toEqual('PharmaSearch');
  });
+
+it('displays all results when no search term is entered', function() {
+  browser.get('http://localhost:8080');
+  expect(resultslist.getText()).toContain("LloydsPharmacy")
+})
+
 });
